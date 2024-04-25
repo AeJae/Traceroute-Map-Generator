dcc1c319705c53a4e4f9c630278179d10d042730
Arun Fletcher <focalpear00@yahoo.co.uk>
2024-04-25 23:19:30 +0100

    Added option to use Ipregistry as IP location source

--- public/js/NetworkManager.js
+++ public/js/NetworkManager.js
@@ -1,9 +1,22 @@
 // Manages all network related tasks.
 export default class NetworkManager {
     allowFetch;
+    useAccount;
+    key = "";
     testLongitude = -2;
-    constructor(enableFetching) {
+    constructor(enableFetching, ipregistryKey) {
+        console.log("AWAIT: Network Manager");
         this.allowFetch = enableFetching;
+        // If an account key was provided...
+        if (typeof ipregistryKey !== 'undefined') {
+            if (!this.allowFetch)
+                console.warn("You have provided your Ipregistry key with live data disabled.");
+            this.useAccount = true;
+            this.key = ipregistryKey;
+        }
+        else {
+            this.useAccount = false;
+        }
         console.log("READY: Network Manager");
     }
     // Returns IP addresses fetched from the server
@@ -23,23 +36,43 @@ export default class NetworkManager {
         return addresses.list;
     }
     // Returns the latitude and longitude of a passed IP address as an array.
-    // Uses the ipapi API (https://ipapi.co/). MAXIMUM OF 30,000 REQUESTS/MONTH.
+    // Uses the ipapi API (https://ipapi.co/).            MAXIMUM OF 30,000 REQUESTS/MONTH.
+    // Or the Ipregistry API (https://ipregistry.co/).    REQUIRES ACCOUNT. 100,000 FREE CREDITS.
     async getIPLocation(ip) {
         if (this.allowFetch) {
-            console.log(`Fetching...`);
-            const rawData = await fetch(`https://ipapi.co/${ip}/json/`);
-            const data = await rawData.json();
-            if (!data.error) {
-                console.log("Complete.");
-                return [data.latitude, data.longitude];
+            // Whether to use Ipregistry or ipapi
+            // Ipregistry
+            if (this.useAccount) {
+                console.log("Fetching with Ipregistry...");
+                const rawData = await fetch(`https://api.ipregistry.co/${ip}?key=${this.key}`);
+                const data = await rawData.json();
+                if (data.location.latitude && data.location.longitude) {
+                    console.log("Complete.");
+                    return [data.location.latitude, data.location.longitude];
+                }
+                else {
+                    console.error("No location data found.");
+                    return [];
+                }
+                // ipapi
             }
             else {
-                console.error(`No location for this IP address. Reason: ${data.reason}`);
-                return [];
+                console.log("Fetching with Ipapi...");
+                const rawData = await fetch(`https://ipapi.co/${ip}/json/`);
+                const data = await rawData.json();
+                if (!data.error) {
+                    console.log("Complete.");
+                    return [data.latitude, data.longitude];
+                }
+                else {
+                    console.error(`No location for this IP address. Reason: ${data.reason}`);
+                    return [];
+                }
             }
+            // Live data disabled
         }
         else {
-            console.error("LIVE DATA FETCHING DISABLED.");
+            console.warn("Live data is disabled.");
             this.testLongitude += 4;
             return [52, this.testLongitude];
         }
--- src/NetworkManager.ts
+++ src/NetworkManager.ts
@@ -1,10 +1,21 @@
 // Manages all network related tasks.
 export default class NetworkManager {
     private readonly allowFetch: boolean;
+    private readonly useAccount: boolean;
+    private readonly key: string = "";
     private testLongitude: number = -2;
 
-    public constructor(enableFetching: boolean) {
+    public constructor(enableFetching: boolean, ipregistryKey?: string) {
+        console.log("AWAIT: Network Manager");
         this.allowFetch = enableFetching;
+        // If an account key was provided...
+        if (typeof ipregistryKey !== 'undefined') {
+            if (!this.allowFetch) console.warn("You have provided your Ipregistry key with live data disabled.");
+            this.useAccount = true;
+            this.key = ipregistryKey;
+        } else {
+            this.useAccount = false;
+        }
         console.log("READY: Network Manager");
     }
 
@@ -27,21 +38,39 @@ export default class NetworkManager {
     }
 
     // Returns the latitude and longitude of a passed IP address as an array.
-    // Uses the ipapi API (https://ipapi.co/). MAXIMUM OF 30,000 REQUESTS/MONTH.
+    // Uses the ipapi API (https://ipapi.co/).            MAXIMUM OF 30,000 REQUESTS/MONTH.
+    // Or the Ipregistry API (https://ipregistry.co/).    REQUIRES ACCOUNT. 100,000 FREE CREDITS.
     public async getIPLocation(ip: string): Promise<any[]> {
         if (this.allowFetch) {
-            console.log(`Fetching...`);
-            const rawData: Response = await fetch(`https://ipapi.co/${ip}/json/`);
-            const data = await rawData.json();
-            if (!data.error) {
-                console.log("Complete.");
-                return [data.latitude, data.longitude];
+            // Whether to use Ipregistry or ipapi
+            // Ipregistry
+            if (this.useAccount) {
+                console.log("Fetching with Ipregistry...");
+                const rawData: Response = await fetch(`https://api.ipregistry.co/${ip}?key=${this.key}`);
+                const data = await rawData.json();
+                if (data.location.latitude && data.location.longitude) {
+                    console.log("Complete.");
+                    return [data.location.latitude, data.location.longitude]
+                } else {
+                    console.error("No location data found.");
+                    return [];
+                }
+            // ipapi
             } else {
-                console.error(`No location for this IP address. Reason: ${data.reason}`);
-                return [];
+                console.log("Fetching with Ipapi...");
+                const rawData: Response = await fetch(`https://ipapi.co/${ip}/json/`);
+                const data = await rawData.json();
+                if (!data.error) {
+                    console.log("Complete.");
+                    return [data.latitude, data.longitude];
+                } else {
+                    console.error(`No location for this IP address. Reason: ${data.reason}`);
+                    return [];
+                }
             }
+        // Live data disabled
         } else {
-            console.error("LIVE DATA FETCHING DISABLED.")
+            console.warn("Live data is disabled.")
             this.testLongitude += 4;
             return [52, this.testLongitude];
         }
